bbed1f54166ed949c32f640c62e590e3f153a1ea
brianwu33 <brianwu20020303@gmail.com>
2022-11-06 21:14:34 -0500

    Initialized backend using spring Initializr

---
+++ the-glenn-project-api/src/main/java/com/brian/theglennprojectapi/TheGlennProjectApiApplication.java
@@ -0,0 +1,13 @@
+package com.brian.theglennprojectapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheGlennProjectApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheGlennProjectApiApplication.class, args);
+	}
+
+}
---
+++ the-glenn-project-api/src/test/java/com/brian/theglennprojectapi/TheGlennProjectApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brian.theglennprojectapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheGlennProjectApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
