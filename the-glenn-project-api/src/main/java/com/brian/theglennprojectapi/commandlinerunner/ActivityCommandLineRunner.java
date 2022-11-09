35106f9dedec6e8407c35529cd1ab0230e6209e6
brianwu33 <brianwu20020303@gmail.com>
2022-11-09 00:21:01 -0500

    Added static data into the h2 database
    1. Created a command line runner class for both the UserDetail and Activity.
    2. Added data into the database for testing purpose

---
+++ the-glenn-project-api/src/main/java/com/brian/theglennprojectapi/commandlinerunner/ActivityCommandLineRunner.java
@@ -0,0 +1,40 @@
+package com.brian.theglennprojectapi.commandlinerunner;
+
+import com.brian.theglennprojectapi.entity.Activity;
+import com.brian.theglennprojectapi.entity.UserDetails;
+import com.brian.theglennprojectapi.repository.ActivityRepository;
+import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.util.List;
+import java.util.UUID;
+
+@AllArgsConstructor
+@Component
+public class ActivityCommandLineRunner implements CommandLineRunner {
+    private ActivityRepository repository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        //Date date1 = Date.valueOf(LocalDate.of(2002, 03, 03));
+        LocalDateTime startTime = LocalDateTime.of(2015, Month.JULY, 29, 19, 30, 40);
+        LocalDateTime endTime = LocalDateTime.of(2015, Month.JULY, 29, 20, 30, 40);
+
+        repository.save(new Activity(UUID.randomUUID(), "Badminton", UUID.randomUUID(), "CIF", startTime, endTime, "aaa", 15));
+        repository.save(new Activity(UUID.randomUUID(), "Basketball", UUID.randomUUID(), "CIF", startTime, endTime, "bbb", 20));
+
+        List<Activity> activities = repository.findAll();
+
+        //List<UserDetails> users = repository.findByRole("Admin");
+
+        Logger logger = LoggerFactory.getLogger(getClass());
+        //users.forEach(user -> logger.info(user.toString()));
+        System.out.println("HI");
+        activities.forEach(activity -> System.out.println(activity.toString()));
+    }
+}
---
+++ the-glenn-project-api/src/main/java/com/brian/theglennprojectapi/commandlinerunner/UserDetailsCommandLineRunner.java
@@ -0,0 +1,39 @@
+package com.brian.theglennprojectapi.commandlinerunner;
+
+import com.brian.theglennprojectapi.entity.UserDetails;
+import com.brian.theglennprojectapi.repository.UserDetailsRepository;
+import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.UUID;
+
+@AllArgsConstructor
+@Component
+public class UserDetailsCommandLineRunner implements CommandLineRunner {
+    private UserDetailsRepository repository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        //Date date1 = Date.valueOf(LocalDate.of(2002, 03, 03));
+        LocalDate date1 = LocalDate.of(2002, 3, 3);
+        LocalDate date2 = LocalDate.of(2000, 10, 25);
+        LocalDate date3 = LocalDate.of(1999, 5, 5);
+
+        repository.save(new UserDetails(UUID.randomUUID(), "Brian", "Wu", "male", date1));
+        repository.save(new UserDetails(UUID.randomUUID(),"Gabriel", "Diniz", "male", date2));
+        repository.save(new UserDetails(UUID.randomUUID(),"Steven", "Wang", "male", date3));
+
+        List<UserDetails> users = repository.findAll();
+
+        //List<UserDetails> users = repository.findByRole("Admin");
+
+        Logger logger = LoggerFactory.getLogger(getClass());
+        //users.forEach(user -> logger.info(user.toString()));
+        System.out.println("HI");
+        users.forEach(user -> System.out.println(user.getId()));
+    }
+}
