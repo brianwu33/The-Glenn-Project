3011a21d058de1ca693f3d377b5b842abc790517
brianwu33 <brianwu20020303@gmail.com>
2022-11-11 22:21:51 -0500

    Added Endpoints in UserDetails Controller

--- the-glenn-project-api/src/main/java/com/brian/theglennprojectapi/controller/UserDetailsController.java
+++ the-glenn-project-api/src/main/java/com/brian/theglennprojectapi/controller/UserDetailsController.java
@@ -3,8 +3,9 @@ package com.brian.theglennprojectapi.controller;
 import com.brian.theglennprojectapi.entity.UserDetails;
 import com.brian.theglennprojectapi.service.UserDetailService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -12,10 +13,24 @@ import java.util.List;
 @RequestMapping("/users")
 public class UserDetailsController {
     @Autowired
-    UserDetailService userService;
+    UserDetailService userDetailService;
 
-    public List<UserDetails> retrieveAllUsers(){
-        return null;
+    @GetMapping()
+    public ResponseEntity<List<UserDetails>> retrieveAllUsers(){
+        return ResponseEntity.status(HttpStatus.OK).body(userDetailService.retrieveAllUsers());
     }
 
+    @GetMapping("/{userId}")
+    public ResponseEntity<UserDetails> retrieveUserById(@PathVariable String userId){
+        return ResponseEntity.status(HttpStatus.OK).body(userDetailService.retrieveUserById(userId));
+    }
+
+    @PutMapping("/{userId}")
+    public ResponseEntity<UserDetails> updateUserById(@PathVariable String userId){
+        return ResponseEntity.status(HttpStatus.CREATED).body(userDetailService.updateUserById(userId));
+    }
+    @DeleteMapping("/{userId}")
+    public ResponseEntity<UserDetails> deleteUserById(@PathVariable String userId){
+        return ResponseEntity.status(HttpStatus.OK).body(userDetailService.deleteUserById(userId));
+    }
 }
--- the-glenn-project-api/src/main/java/com/brian/theglennprojectapi/service/UserDetailService.java
+++ the-glenn-project-api/src/main/java/com/brian/theglennprojectapi/service/UserDetailService.java
@@ -1,7 +1,25 @@
 package com.brian.theglennprojectapi.service;
 
+import com.brian.theglennprojectapi.entity.UserDetails;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class UserDetailService {
+    public List<UserDetails> retrieveAllUsers() {
+        return null;
+    }
+
+    public UserDetails retrieveUserById(String userId) {
+        return null;
+    }
+
+    public UserDetails updateUserById(String userId) {
+        return null;
+    }
+
+    public UserDetails deleteUserById(String userId) {
+        return null;
+    }
 }
