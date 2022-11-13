104490bcf54b1b7e3d5fc7a0c6c11648b88b20b0
brianwu33 <brianwu20020303@gmail.com>
2022-11-13 11:14:49 -0500

    Added API Versioning
    Added API Versioning through the URI path

--- the-glenn-project-api/src/main/java/com/brian/theglennprojectapi/controller/ActivityController.java
+++ the-glenn-project-api/src/main/java/com/brian/theglennprojectapi/controller/ActivityController.java
@@ -1,7 +1,9 @@
 package com.brian.theglennprojectapi.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/v1/activities")
 public class ActivityController {
 }
