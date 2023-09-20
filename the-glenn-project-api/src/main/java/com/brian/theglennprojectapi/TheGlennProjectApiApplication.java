cad83eb9df2ec24cadece3a28494f81ddc34ffce
Brian Wu <byhwu@uwaterloo.ca>
2023-09-19 21:14:04 -0400

    Configured Swagger3 for API Documentation

--- the-glenn-project-api/src/main/java/com/brian/theglennprojectapi/TheGlennProjectApiApplication.java
+++ the-glenn-project-api/src/main/java/com/brian/theglennprojectapi/TheGlennProjectApiApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TheGlennProjectApiApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(TheGlennProjectApiApplication.class, args);
 	}
