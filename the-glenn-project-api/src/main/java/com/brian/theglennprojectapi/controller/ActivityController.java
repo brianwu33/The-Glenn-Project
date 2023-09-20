7fe9f6c52868c379fa565bb5854fe7e75b1dc818
Brian Wu <byhwu@uwaterloo.ca>
2023-09-19 21:53:30 -0400

    Added Swagger Annotations
    Added Swagger Annotations that summarize each endpoint in the documentation

--- the-glenn-project-api/src/main/java/com/brian/theglennprojectapi/controller/ActivityController.java
+++ the-glenn-project-api/src/main/java/com/brian/theglennprojectapi/controller/ActivityController.java
@@ -5,6 +5,7 @@ import com.brian.theglennprojectapi.dto.ActivityResponseDTO;
 import com.brian.theglennprojectapi.exception.ActivityNotFoundException;
 import com.brian.theglennprojectapi.exception.UserNotFoundException;
 import com.brian.theglennprojectapi.service.ActivityService;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,40 +21,48 @@ public class ActivityController {
     @Autowired
     ActivityService activityService;
 
+    @Operation(summary = "Retrieve All Activities")
     @GetMapping()
     public ResponseEntity<List<ActivityResponseDTO>> retrieveAllActivities(){
         return ResponseEntity.status(HttpStatus.OK).body(activityService.retrieveAllActivities());
     }
 
+    @Operation(summary = "Create Activity")
     @PostMapping()
     public ResponseEntity<ActivityResponseDTO> createActivity(@RequestBody ActivityRequestDTO activityRequestDTO){
         ActivityResponseDTO response = activityService.createActivity(activityRequestDTO);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
+
+    @Operation(summary = "Retrieve Activity By ID")
     @GetMapping("/{activityId}")
     public ResponseEntity<ActivityResponseDTO> retrieveActivityById(@PathVariable Long activityId){
         ActivityResponseDTO response = activityService.retrieveActivityById(activityId);
         return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 
+    @Operation(summary = "Update Activity By ID")
     @PutMapping("/{activityId}")
     public ResponseEntity<ActivityResponseDTO> updateActivityById(@PathVariable Long activityId, @RequestBody ActivityRequestDTO activityRequestDTO){
         ActivityResponseDTO response = activityService.updateActivityById(activityId, activityRequestDTO);
         return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 
+    @Operation(summary = "Delete Activity By ID")
     @DeleteMapping("/{activityId}")
     public ResponseEntity<ActivityResponseDTO> deleteActivityById(@PathVariable Long activityId){
         ActivityResponseDTO response = activityService.deleteActivityById(activityId);
         return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 
+    @Operation(summary = "Add Activity Participants By ID")
     @PostMapping("/{activityId}/participants/{userId}")
     public ResponseEntity<ActivityResponseDTO> addActivityParticipants(@PathVariable Long activityId, @PathVariable Long userId) throws Exception{
         ActivityResponseDTO response = activityService.addActivityParticipants(activityId, userId);
         return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 
+    @Operation(summary = "Delete Activity Participants By ID")
     @DeleteMapping("/{activityId}/participants/{userId}")
     public ResponseEntity<ActivityResponseDTO> deleteActivityParticipants(@PathVariable Long activityId, @PathVariable Long userId) throws Exception{
         ActivityResponseDTO response = activityService.deleteActivityParticipants(activityId, userId);
--- the-glenn-project-api/src/main/java/com/brian/theglennprojectapi/controller/UserDetailsController.java
+++ the-glenn-project-api/src/main/java/com/brian/theglennprojectapi/controller/UserDetailsController.java
@@ -5,6 +5,7 @@ import com.brian.theglennprojectapi.dto.UserDetailsRequestDTO;
 import com.brian.theglennprojectapi.dto.UserDetailsResponseDTO;
 import com.brian.theglennprojectapi.service.UserDetailService;
 import com.brian.theglennprojectapi.exception.UserNotFoundException;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,39 +20,46 @@ public class UserDetailsController {
     @Autowired
     UserDetailService userDetailService;
 
+    @Operation(summary = "Retrieve All Users")
     @GetMapping()
     public ResponseEntity<List<UserDetailsResponseDTO>> retrieveAllUsers(){
         return ResponseEntity.status(HttpStatus.OK).body(userDetailService.retrieveAllUsers());
     }
 
+    @Operation(summary = "Retrieve User By ID")
     @GetMapping("/{userId}")
     public ResponseEntity<UserDetailsResponseDTO> retrieveUserById(@PathVariable Long userId){
         UserDetailsResponseDTO user = userDetailService.retrieveUserById(userId);
         return ResponseEntity.status(HttpStatus.OK).body(user);
     }
+    @Operation(summary = "Create User")
+
     @PostMapping()
     public ResponseEntity<UserDetailsResponseDTO> createUser(@RequestBody UserDetailsRequestDTO userDetailsRequestDTO){
         return ResponseEntity.status(HttpStatus.CREATED).body(userDetailService.createUser(userDetailsRequestDTO));
     }
 
+    @Operation(summary = "Update User By ID")
     @PutMapping("/{userId}")
     public ResponseEntity<UserDetailsResponseDTO> updateUserById(@PathVariable Long userId, @RequestBody UserDetailsRequestDTO userDetailsRequestDTO){
         UserDetailsResponseDTO user = userDetailService.updateUserById(userId, userDetailsRequestDTO);
         return ResponseEntity.status(HttpStatus.OK).body(user);
     }
+    @Operation(summary = "Delete User By ID")
     @DeleteMapping("/{userId}")
     public ResponseEntity<UserDetailsResponseDTO> deleteUserById(@PathVariable Long userId) throws Exception{
         UserDetailsResponseDTO user = userDetailService.deleteUserById(userId);
         return ResponseEntity.status(HttpStatus.OK).body(user);
     }
 
+    @Operation(summary = "Retrieve all joined activities By User ID")
     @GetMapping("/{userId}/activities/joined-activities")
     public ResponseEntity<List<ActivityResponseDTO>> retrieveJoinedActivityByUserId(@PathVariable Long userId){
         List<ActivityResponseDTO> activities = userDetailService.retrieveJoinedActivityByUserId(userId);
         return ResponseEntity.status(HttpStatus.OK).body(activities);
     }
 
-    @GetMapping("/{userId}/activities/created-activities")
+    @Operation(summary = "Retrieve all created activities By User ID")
     public ResponseEntity<List<ActivityResponseDTO>> retrieveCreatedActivityByUserId(@PathVariable Long userId){
         List<ActivityResponseDTO> activities = userDetailService.retrieveCreatedActivityByUserId(userId);
         return ResponseEntity.status(HttpStatus.OK).body(activities);
